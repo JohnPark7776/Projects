11f94812db8d2a16a5905097d9d61921d565bc4f
John Park <99522615+JohnPark7776@users.noreply.github.com>
2023-03-19 21:11:03 -1000

    Update Instagram_Link.py

--- DiscordBot/Instagram_Link.py
+++ DiscordBot/Instagram_Link.py
@@ -5,6 +5,7 @@ import requests
 
 
 results = {}
+
 def parse_item(html_page):
     """Parses through a string to grab Img Src or Video Src
 
@@ -14,13 +15,13 @@ def parse_item(html_page):
     soup = bs(html_page, 'html.parser')
     data = soup.find_all('img')
     for item in data:
-        image = requests.get(item['src'])
-        results[image.content] = 'img'
+        if str(item['class']).replace(' ', "") != "['xh8yej3','xl1xv1r','x5yr21d']":
+            image = requests.get(item['src'])
+            results[image.content] = 'img'
     data = soup.find_all('video')
     for item in data:
         video = requests.get(item['src'])
         results[video.content] = 'vid'
-
 
 async def start_this(link):
     """Opens a browser to selected Instagram Post
@@ -41,27 +42,25 @@ async def start_this(link):
         await page.goto(url, wait_until='networkidle')
         try:
             if await page.is_enabled('div._aao_',timeout=5000):
+                main = page.locator('div._aao_')
                 for i in range(10):
                     try:
-                        next_page = page.locator('div._aao_').get_by_role('button',name='Next')
+                        next_page = main.get_by_role('button',name='Next')
                         page.set_default_timeout(500)
-                        for li in await page.locator('div._aao_').filter(has=page.get_by_role('listitem')).all():
+                        for li in await main.locator('ul._acay').all():
                             parse_item(await li.inner_html())
                         await next_page.click()
                     except TE:
                         break
             await browser.close()
+            return results
         except TE:
             await page.reload(wait_until='networkidle')
             try:
-                content = await page.locator('article').locator('div._aagv').inner_html()
-            except:
-                content = await page.locator('div._aagv').inner_html()
-            parse_item(await content)
-            try:
-                content = await page.locator('x5yr21d x1uhb9sk xh8yej3').inner_html()
-            except:
-                pass
+                if await page.is_enabled('div._aagv',timeout=1000):
+                    content = await page.locator('div._aagv').inner_html()
+            except TE:
+                content = await page.locator('div.x5yr21d x1uhb9sk xh8yej3').inner_html()
             parse_item(await content)
             await browser.close()
-    return results
+            return results
